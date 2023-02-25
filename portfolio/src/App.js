07065cf068093900be9ab5b72348e2e4b18658e7
ball712004 <nguyenminhtam7124@gmail.com>
2023-02-24 21:29:40 -0500

    Update React UI

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Tam Nguyen</title>
+      </head>
+      <body>
+        <h1>Portfolio</h1>
+        <p>Test</p>
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
